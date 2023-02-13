a79cd44ebff7ad22d610901be1e42f6469465d59
Mauricio Ochoa <112920459+MauricioOchoa@users.noreply.github.com>
2023-02-13 13:34:33 -0800

    Unity_test
    Code for testing the scene transition

---
+++ SceneTransition/Scene_transition/Assets/Scripts/ScriptableObject/VectorValue.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class VectorValue : ScripteableObjects 
+{
+    public Vector2 initialValue;
+}
